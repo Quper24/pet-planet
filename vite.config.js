0efd2c527c70e2528529271b4f645b302bf62021
Maksim Leskin <maskim.leskin@methed.ru>
2024-04-17 19:55:34 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: './src',
-  publicDir: '../public',
+  root: "./src",
+  publicDir: "../public",
   build: {
-    outDir: '../dist',
-  },
+    outDir: "../dist"
+  }
 });
