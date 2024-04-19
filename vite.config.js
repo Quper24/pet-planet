302a050edf33128f10149a5ec4a0a4d46c29be78
Maksim Leskin <maskim.leskin@methed.ru>
2024-04-19 20:05:31 +0300

    optimize

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   root: "./src",
@@ -6,4 +7,23 @@ export default defineConfig({
   build: {
     outDir: "../dist",
   },
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+      avif: {
+        quality: 70,
+      },
+    }),
+  ],
 });
